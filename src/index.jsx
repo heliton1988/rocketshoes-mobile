ba0905dab636102fc6ddba017eb5a14f541223d8
Héliton Oliveira <heliton.oliveira88@gmail.com>
2020-08-17 17:14:14 -0300

    :rocket: Initial commit

---
+++ src/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+import './config/ReactotronConfig';
+
+function App() {
+  return (
+    <>
+      <View>
+        <Text>Hello Dev!!!3</Text>
+      </View>
+    </>
+  );
+}
+
+export default App;
