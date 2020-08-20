1110d22deec0ffca9eb6c9ed93d90ca765eb2e78
Héliton Oliveira <heliton.oliveira88@gmail.com>
2020-08-20 14:59:18 -0300

    feat: api consumption and List sylized card of products

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
-import {Text} from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
-import {Container, Card} from './styles';
+import api from '../../services/api';
+
+import {
+  Container,
+  CardScrollContainer,
+  CardContainer,
+  CardBox,
+  CardImage,
+  CardText,
+  CardPrice,
+  CardButton,
+  CardButtonText,
+  CardQuatityContainer,
+  CardQuatity,
+} from './styles';
 
 class Home extends React.Component {
   constructor(props) {
@@ -11,12 +25,39 @@ class Home extends React.Component {
     };
   }
 
+  async componentDidMount() {
+    const response = await api.get(`products`);
+
+    const products = response.data;
+
+    this.setState({products});
+  }
+
   render() {
     const {products} = this.state;
 
     return (
       <Container>
-        <Card />
+        <CardScrollContainer
+          data={products}
+          keyExtractor={product => String(product.id)}
+          renderItem={({item}) => (
+            <CardContainer key={item.id}>
+              <CardBox>
+                <CardImage source={{uri: item.image}} />
+                <CardText>{item.title}</CardText>
+                <CardPrice>R$ {item.price}</CardPrice>
+                <CardButton onPress={() => {}}>
+                  <CardQuatityContainer>
+                    <Icon name="shopping-cart" size={20} color="#fff" />
+                    <CardQuatity>1</CardQuatity>
+                  </CardQuatityContainer>
+                  <CardButtonText>ADICIONAR</CardButtonText>
+                </CardButton>
+              </CardBox>
+            </CardContainer>
+          )}
+        />
       </Container>
     );
   }
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,11 +1,80 @@
 import styled from 'styled-components/native';
 import {FlatList} from 'react-native';
+import {RectButton} from 'react-native-gesture-handler';
 import colors from '../../styles/colors';
 
 export const Container = styled.View`
   flex: 1;
-  padding: 30px;
   background: ${colors.secondary};
+  padding-top: 30px;
 `;
 
-export const Card = styled(FlatList)``;
+export const CardScrollContainer = styled(FlatList).attrs({
+  horizontal: true,
+  showsHorizontalScrollIndicator: false,
+})``;
+
+export const CardContainer = styled.View`
+  width: 220px;
+  height: 358px;
+  margin-top: 20px;
+  margin-right: 15px;
+  border-radius: 4px;
+
+  background: #fff;
+
+  align-items: center;
+`;
+
+export const CardBox = styled.View``;
+
+export const CardImage = styled.Image`
+  width: 200px;
+  height: 200px;
+`;
+
+export const CardText = styled.Text.attrs({
+  numberOfLines: 2,
+})`
+  width: 186px;
+  color: #333333;
+  font-size: 16px;
+  margin-top: 10px;
+`;
+
+export const CardPrice = styled.Text`
+  font-size: 21px;
+  font-weight: bold;
+  color: #000;
+  margin-top: 5px;
+`;
+
+export const CardButton = styled(RectButton)`
+  flex-direction: row;
+  align-items: center;
+  background: #7159c1;
+  border-radius: 4px;
+  margin-top: 14px;
+`;
+
+export const CardButtonText = styled.Text`
+  flex: 1;
+  text-align: center;
+  font-weight: bold;
+
+  color: #fff;
+`;
+
+export const CardQuatityContainer = styled.View`
+  flex-direction: row;
+  align-items: center;
+
+  padding: 13px;
+  background: rgba(0, 0, 0, 0.1);
+`;
+
+export const CardQuatity = styled.Text`
+  font-size: 14px;
+  color: #fff;
+  padding-left: 5px;
+`;
