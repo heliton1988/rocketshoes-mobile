8bf3f2e52a95c4aef748bc02bc17ce1c2089797e
Héliton Oliveira <heliton.oliveira88@gmail.com>
2020-08-21 11:46:04 -0300

    :tada: feat: cart screen layout is done

--- src/styles/colors.js
+++ src/styles/colors.js
@@ -1,6 +1,9 @@
 export default {
   primary: '#7159c1',
   white: '#ffffff',
+  lightGray: '#EEEEEE',
+  darkGray: '#999999',
+  gray: '#979797',
   textDescription: '#333333',
   secondary: '#191920',
   dark: '#141419',
